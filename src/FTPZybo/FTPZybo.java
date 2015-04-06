17bad1bd026bec06560ca156d1058ff721ccddba
Niko <nikooftheworld@gmail.com>
2015-04-06 13:58:02 +0200

    Tilføjet henvisning til FTPKlient

--- src/FTPZybo/FTPZybo.java
+++ src/FTPZybo/FTPZybo.java
@@ -2,19 +2,21 @@ package FTPZybo;
 
 public class FTPZybo {
 	
-	private TUI  tui;
-	private Zybo zybo;
+	private TUI		  tui;
+	private Zybo	  zybo;
+	private FTPClient ftp;
 
 	public static void main(String[] args) {
 		
-		FTPZybo fz = new FTPZybo(new TUI(), new Zybo());
+		FTPZybo fz = new FTPZybo(new TUI(), new Zybo(), new FTPClient());
 
 	}
 	
-	public FTPZybo(TUI tui, Zybo zybo)
+	public FTPZybo(TUI tui, Zybo zybo, FTPClient ftp)
 	{
 		this.tui  = tui;
 		this.zybo = zybo;
+		this.ftp  = ftp;
 		
 		operations(tui.printMenu());
 	}
--- src/FTPZybo/TUI.java
+++ src/FTPZybo/TUI.java
@@ -10,21 +10,23 @@ public class TUI {
 	public int printMenu()
 	{
 		System.out.println("***********************************************");
-		System.err.println("*                                             *");
+		System.out.println("*                                             *");
 		System.out.println("* Super sej FTP/Zybo-ting                     *");
-		System.err.println("*                                             *");
+		System.out.println("*                                             *");
 		System.out.println("***********************************************");
-		System.err.println("*                                             *");
-		System.err.println("* Menu:                                       *");
-		System.err.println("*                                             *");
-		System.err.println("* 1. List filer på FTP                    ... *");
-		System.err.println("*                                             *");
-		System.err.println("* 2. Skriv til sensor                     ... *");
-		System.err.println("* 3. Læs fra sensor nr.                   ... *");
-		System.err.println("* 4. Start sensor                         ... *");
-		System.err.println("* 5. Stop sensor                          ... *");
-		System.err.println("* 6. List sensorer                            *");
-		System.err.println("*                                             *");
+		System.out.println("*                                             *");
+		System.out.println("* Menu:                                       *");
+		System.out.println("*                                             *");
+		System.out.println("* 1. List filer på FTP                    ... *");
+		System.out.println("*                                             *");
+		System.out.println("* 2. Skriv til sensor                     ... *");
+		System.out.println("* 3. Læs fra sensor nr.                   ... *");
+		System.out.println("* 4. Start sensor                         ... *");
+		System.out.println("* 5. Stop sensor                          ... *");
+		System.out.println("* 6. List sensorer                            *");
+		System.out.println("*                                             *");
+		System.out.println("* 0. Afslut program                           *");
+		System.out.println("*                                             *");
 		System.out.println("***********************************************");
 		
 		System.out.println("Indtast valg: ");		
