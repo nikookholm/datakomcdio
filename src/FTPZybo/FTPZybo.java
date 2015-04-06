10c7fc231b682d8e8803d0ad04a73c59f9c11ed7
Niko <nikooftheworld@gmail.com>
2015-04-06 12:46:35 +0200

    Nye pakker og tomme klasser YAY!

---
+++ src/FTPZybo/FTPZybo.java
@@ -0,0 +1,10 @@
+package FTPZybo;
+
+public class FTPZybo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/WeightControlUnit/WeightControlUnit.java
@@ -0,0 +1,10 @@
+package WeightControlUnit;
+
+public class WeightControlUnit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
