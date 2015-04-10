b0772fef601441f2735ecbfcb3f60a300ffb2053
thommelutten <thomas.m.elbo@gmail.com>
2015-04-10 12:40:41 +0200

    Start på den flotte WeightControlUnit

--- src/WeightControlUnit/WeightControlUnit.java
+++ src/WeightControlUnit/WeightControlUnit.java
@@ -1,10 +1,43 @@
 package WeightControlUnit;
 
+import java.util.Scanner;
+
 public class WeightControlUnit {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    static int cOprNR = 998;
+    static int password = 1234;
+    static Scanner keyPress = new Scanner(System.in);
 
-	}
+    public static void main(String[] args) {
+
+        int OprNR = keyPress.nextInt();
+        int temp_pass = keyPress.nextInt();
+
+        if (login(OprNR, temp_pass) == true) {
+
+        }
+    }
+
+    public static boolean login(int oprNR, int pw) {
+        if ((oprNR == cOprNR) && (pw == password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int checkItem(int itemNumber) {
+        return 0;
+    }
+
+    public void SendInstruction(String instruc) {
+
+    }
+
+    public void registerItem(int amount) {
+
+    }
+
+}
 
 }
